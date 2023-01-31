eb8de9f8df3e142b865dde741c0f65732efab820
sidiraos <raossidi@gmail.com>
2023-01-31 18:09:59 +0300

    Pomodro App V.1.0.0 first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const work = document.querySelector('#work');
+const rest = document.querySelector('#rest');
+const workTime = document.querySelector('#workTime');
+const restTime = document.querySelector('#restTime');
+const playBtn = document.querySelector('#play') ;
+const resetBtn = document.querySelector('#reset') ;
